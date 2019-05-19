6fab7921d872ce1da060af05c126b9cb1be2c137
gaufung <gaufung@outlook.com>
2019-05-19 15:03:01 +0800

    complete 301 and 238

--- Algorithm/238.除自身以外数组的乘积.go
+++ Algorithm/238.除自身以外数组的乘积.go
@@ -15,19 +15,36 @@
  *
  * 给定长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，其中 output[i] 等于 nums 中除 nums[i]
  * 之外其余各元素的乘积。
- * 
+ *
  * 示例:
- * 
+ *
  * 输入: [1,2,3,4]
  * 输出: [24,12,8,6]
- * 
+ *
  * 说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
- * 
+ *
  * 进阶：
  * 你可以在常数空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组不被视为额外空间。）
- * 
+ *
  */
 func productExceptSelf(nums []int) []int {
-    
+	n := len(nums)
+	left := make([]int, n)
+	left[0] = 1
+	for i := 1; i < n; i++ {
+		left[i] = left[i-1] * nums[i-1]
+	}
+	right := make([]int, n)
+	right[n-1] = 1
+	for i := n - 2; i >= 0; i-- {
+		right[i] = right[i+1] * nums[i+1]
+	}
+	result := make([]int, n)
+	for i := 0; i < n; i++ {
+		result[i] = left[i] * right[i]
+	}
+	return result
 }
+
+
 
--- Algorithm/301.删除无效的括号.go
+++ Algorithm/301.删除无效的括号.go
@@ -36,6 +36,70 @@
  *
  */
 func removeInvalidParentheses(s string) []string {
-
+	leftRemove, rightRemove := needToRemove(s)
+	return dfs(s, 0, leftRemove, rightRemove)
 }
 
+func dfs(s string, index int, leftRemove, rightRemove int) []string {
+	if leftRemove == 0 && rightRemove == 0 && valid(s) {
+		return []string{s}
+	}
+	result := make([]string, 0)
+	for i := index; i < len(s); i++ {
+		if s[i] == '(' && leftRemove > 0 {
+			if i > 0 && s[i-1] == '(' {
+				continue
+			}
+			leftResult := dfs(removeKth(s, i), i, leftRemove-1, rightRemove)
+			for _, left := range leftResult {
+				result = append(result, left)
+			}
+		}
+		if s[i] == ')' && rightRemove > 0 {
+			if i > 0 && s[i-1] == ')' {
+				continue
+			}
+			rightResult := dfs(removeKth(s, i), i, leftRemove, rightRemove-1)
+			for _, right := range rightResult {
+				result = append(result, right)
+			}
+		}
+	}
+	return result
+}
+
+func removeKth(s string, i int) string {
+	return s[:i] + s[i+1:]
+}
+
+func needToRemove(s string) (int, int) {
+	left, right := 0, 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			left++
+		} else if s[i] == ')' {
+			if left > 0 {
+				left--
+			} else {
+				right++
+			}
+		}
+	}
+	return left, right
+}
+
+func valid(s string) bool {
+	left, right := 0, 0
+	for i := 0; i < len(s); i++ {
+		if right > left {
+			return false
+		}
+		if s[i] == '(' {
+			left++
+		}
+		if s[i] == ')' {
+			right++
+		}
+	}
+	return left == right
+}
