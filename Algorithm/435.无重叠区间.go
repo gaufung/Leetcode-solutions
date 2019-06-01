dc302885f95195612a4485019f5c5dab33c08d87
gaufung <gaufung@outlook.com>
2019-06-01 17:16:31 +0800

    complete435

--- Algorithm/435.无重叠区间.go
+++ Algorithm/435.无重叠区间.go
@@ -1,3 +1,5 @@
+import "sort"
+
 /*
  * @lc app=leetcode.cn id=435 lang=golang
  *
@@ -14,46 +16,79 @@
  * Testcase Example:  '[[1,2]]'
  *
  * 给定一个区间的集合，找到需要移除区间的最小数量，使剩余区间互不重叠。
- * 
+ *
  * 注意:
- * 
- * 
+ *
+ *
  * 可以认为区间的终点总是大于它的起点。
  * 区间 [1,2] 和 [2,3] 的边界相互“接触”，但没有相互重叠。
- * 
- * 
+ *
+ *
  * 示例 1:
- * 
- * 
+ *
+ *
  * 输入: [ [1,2], [2,3], [3,4], [1,3] ]
- * 
+ *
  * 输出: 1
- * 
+ *
  * 解释: 移除 [1,3] 后，剩下的区间没有重叠。
- * 
- * 
+ *
+ *
  * 示例 2:
- * 
- * 
+ *
+ *
  * 输入: [ [1,2], [1,2], [1,2] ]
- * 
+ *
  * 输出: 2
- * 
+ *
  * 解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
- * 
- * 
+ *
+ *
  * 示例 3:
- * 
- * 
+ *
+ *
  * 输入: [ [1,2], [2,3] ]
- * 
+ *
  * 输出: 0
- * 
+ *
  * 解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
- * 
- * 
+ *
+ *
  */
 func eraseOverlapIntervals(intervals [][]int) int {
-    
+	n := len(intervals)
+	if n <= 0 {
+		return 0
+	}
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] < intervals[j][0] {
+			return true
+		} else if intervals[i][0] > intervals[j][0] {
+			return false
+		} else {
+			return intervals[i][1] < intervals[j][1]
+		}
+	})
+	dp := make([]int, n)
+	dp[0] = 1
+	maxPair := 1
+	for i := 1; i < n; i++ {
+		for j := i - 1; j >= 0; j-- {
+			if intervals[i][0] >= intervals[j][1] {
+				dp[i] = max(dp[i], dp[j]+1)
+			} else {
+				dp[i] = max(dp[i], 1)
+			}
+		}
+		maxPair = max(maxPair, dp[i])
+	}
+	return n - maxPair
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
